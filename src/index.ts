28a3b21c91ab6311a8fc72f51ada17afd7eb3fb5
paulius-ta <paulius.tartilas@gmail.com>
2024-04-27 15:04:24 +0300

    Add cors package + update index.ts with test GET

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/test', (req: Request, res: Response) => {
+  res.json({ test: 'test123' });
 });
 
 app.listen(port, () => {
