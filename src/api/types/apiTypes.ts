f896d42bd5cc94c63bcee24ca09d1a67747ab956
paulius-ta <paulius.tartilas@gmail.com>
2024-05-22 10:47:53 +0300

    Add initial api schemas and types

---
+++ src/api/types/apiTypes.ts
@@ -0,0 +1,14 @@
+import { components } from 'src/api/apiSchemas';
+
+export type SchemaTypes = components['schemas'];
+
+export type Description = SchemaTypes['Description'];
+export type ArtifactAttribute = SchemaTypes['ArtifactAttribute'];
+export type History = SchemaTypes['History'];
+export type HistoryEntry = SchemaTypes['HistoryEntry'];
+export type Images = SchemaTypes['Images'];
+export type ImageUrl = SchemaTypes['ImageUrl'];
+export type PaymentMethods = SchemaTypes['PaymentMethods'];
+export type ContactDetails = SchemaTypes['ContactDetails'];
+export type PaymentDetails = SchemaTypes['PaymentDetails'];
+export type SecurityCodeDetails = SchemaTypes['SecurityCode'];
