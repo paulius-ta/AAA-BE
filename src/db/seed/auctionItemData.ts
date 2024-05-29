062733b64b5b37dcd84b835906e6bc1896c0697f
paulius-ta <paulius.tartilas@gmail.com>
2024-05-29 19:01:09 +0300

    Add auctionItemData & descriptionData

---
+++ src/db/seed/auctionItemData.ts
@@ -0,0 +1,17 @@
+import { sql } from 'drizzle-orm';
+
+enum Status {
+  Pending = 'pending',
+  Finished = 'finished',
+}
+
+export const auctionItemData = [
+  {
+    id: BigInt('90137109120'),
+    minPrice: 0,
+    currentPrice: 100,
+    startTimestamp: sql`CURRENT_TIMESTAMP`,
+    endTimestamp: sql`CURRENT_TIMESTAMP + INTERVAL '1 hour'`,
+    status: Status.Pending,
+  },
+];
---
+++ src/db/seed/descriptionData.ts
@@ -0,0 +1,21 @@
+export const descriptionData = [
+  {
+    auctionItemId: BigInt('90137109120'),
+    details: {
+      origin: ['unknown', 'mixed'],
+      number: 4,
+      condition: 'good',
+      dimensions: [400, 700],
+      attributes: [
+        { id: 'BKQLEYF', type: '#00FF82' },
+        { id: 'LHSDFV', type: '#FF8E00' },
+        { id: 'XDFR', type: '#0140FF' },
+        { id: 'OUBXMK', type: '#FF00F1' },
+        { id: 'ZHJIO', type: '#FF4300' },
+        { id: 'EGHKI', type: '#FFD801' },
+        { id: 'RLDFP', type: '#01FFC6' },
+        { id: 'ASDFYUI', type: '#8400FF' },
+      ],
+    },
+  },
+];
