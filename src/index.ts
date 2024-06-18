88306c3096fef13f0a623190725f53b8e7ecff29
paulius-ta <paulius.tartilas@gmail.com>
2024-06-18 16:33:10 +0300

    Add initial GET for auctionItem/:id route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { db } from './db/db';
+import { auctionItemTable } from './db/schema';
+import { eq } from 'drizzle-orm';
 
 dotenv.config();
 
@@ -15,6 +18,14 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/test', (req: Request, res: Response) => {
   res.json({ test: 'test123' });
+});
+
+app.get('/auctionItem/:id', async (req: Request, res: Response) => {
+  const auctionItem = await db
+    .select()
+    .from(auctionItemTable)
+    .where(eq(auctionItemTable.id, Number(req.params.id)));
+  res.json({ data: auctionItem });
 });
 
 app.listen(port, () => {
