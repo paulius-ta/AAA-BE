51f4f8f8d648535d76523f2e3005ca17733b2137
paulius-ta <paulius.tartilas@gmail.com>
2024-06-26 15:51:15 +0300

    Add initial descriptionRoutes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import auctionItemRoutes from 'src/routes/auctionItemRoutes';
+import descriptionRoutes from 'src/routes/descriptionRoutes';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/auctionItem', auctionItemRoutes);
+app.use('/description', descriptionRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/descriptionRoutes.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import { db } from 'src/db/db';
+import { descriptionTable } from 'src/db/schema';
+import { eq } from 'drizzle-orm';
+
+const router = express.Router();
+
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const description = await db
+      .select()
+      .from(descriptionTable)
+      .where(eq(descriptionTable.auctionItemId, Number(req.params.id)));
+
+    if (description) {
+      res.status(200).json({ data: description });
+    } else {
+      res.status(404).json({ message: 'Description not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching auction item description:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+export default router;
