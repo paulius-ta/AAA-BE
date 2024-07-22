122e2e26a85244cff0e4aeb2257185e43a766b0d
paulius-ta <paulius.tartilas@gmail.com>
2024-07-22 17:16:50 +0300

    Update delete operations order + add more reset sequences to seed.ts

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,5 @@
 import { db } from 'src/db/db';
-import { auctionItemTable, descriptionTable, historyTable, bidderTable } from 'src/db/schema';
+import { auctionItemTable, descriptionTable, historyTable, bidderTable, imageTable } from 'src/db/schema';
 import { auctionItemData } from 'src/db/seed/auctionItemData';
 import { descriptionData } from 'src/db/seed/descriptionData';
 import { bidderData } from 'src/db/seed/bidderData';
@@ -8,21 +8,30 @@ import { sql } from 'drizzle-orm';
 
 const main = async () => {
   try {
-    await db.delete(auctionItemTable);
-    await db.delete(descriptionTable);
+    console.log('Deleting existing data...');
     await db.delete(historyTable);
+    await db.delete(imageTable);
+    await db.delete(descriptionTable);
+    await db.delete(auctionItemTable);
     await db.delete(bidderTable);
 
+    console.log('Inserting new data...');
+    await db.insert(bidderTable).values(bidderData);
     await db.insert(auctionItemTable).values(auctionItemData);
     await db.insert(descriptionTable).values(descriptionData);
-    await db.insert(bidderTable).values(bidderData);
     await db.insert(historyTable).values(historyData);
 
-    await db.execute(sql`
-      SELECT setval(pg_get_serial_sequence('bidder', 'id'), (SELECT MAX(id) FROM bidder));
-    `);
+    console.log('Resetting sequences...');
+    await db.execute(sql`SELECT setval(pg_get_serial_sequence('bidder', 'id'), (SELECT MAX(id) FROM bidder))`);
+    await db.execute(
+      sql`SELECT setval(pg_get_serial_sequence('auction_item', 'id'), (SELECT MAX(id) FROM auction_item))`,
+    );
+    await db.execute(
+      sql`SELECT setval(pg_get_serial_sequence('description', 'id'), (SELECT MAX(id) FROM description))`,
+    );
+    await db.execute(sql`SELECT setval(pg_get_serial_sequence('history', 'id'), (SELECT MAX(id) FROM history))`);
 
-    console.log('Database seeded successfully');
+    console.log('Database seeded successfully and sequences reset');
   } catch (error) {
     console.error('Error seeding database:', error);
   }
